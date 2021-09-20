e388adbf89540fbb083a22f63e49303d22d1f144
uffoltzl <61827376+uffoltzl@users.noreply.github.com>
2021-09-20 22:02:01 +0200

    demo

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# object_recognition_RN
+# How to recognise real time object in React Native for dummies
+
+This repository goes hand in hand with an article that you can find here.
+
+To launch the demo,
+```
+cd demo
+npm i
+expo start
+```
---
+++ demo/App.tsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as tf from "@tensorflow/tfjs";
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
+
+interface Dimensions {
+  height: number,
+  width: number,
+} 
+
+let textureDims: Dimensions;
+if (Platform.OS === 'ios') {
+  textureDims = {
+    height: 1920,
+    width: 1080,
+  };
+ } else {
+  textureDims = {
+    height: 1200,
+    width: 1600,
+  };
+}
+
+const TensorCamera = cameraWithTensors(Camera);
+let net: mobilenet.MobileNet;
+
+const loadModel = async () => {
+  await tf.ready();
+  tf.getBackend();
+  net = await mobilenet.load();
+}
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState<null | boolean>(null);
+  const [detections, setDetections] = useState<string[]>([]);
+
+
+  const handleCameraStream = (images: IterableIterator<tf.Tensor3D>) => {
+    const loop = async () => {
+      const nextImageTensor = images.next().value;
+      const objects = await net.classify(nextImageTensor);
+      if(objects && objects.length > 0){
+        setDetections(objects.map(object => object.className));
+      }
+      requestAnimationFrame(loop);
+    }
+    loop();
+  }
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+      await loadModel();
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  if(net === null){
+    return <Text>Model not loaded</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <TensorCamera 
+        style={styles.camera} 
+        onReady={handleCameraStream}
+        type={Camera.Constants.Type.back}
+        cameraTextureHeight={textureDims.height}
+        cameraTextureWidth={textureDims.width}
+        resizeHeight={200}
+        resizeWidth={152}
+        resizeDepth={3}
+        autorender={true}
+      />
+      <View style={styles.text}>
+      {detections.map((detection, index) => 
+          <Text key={index}>{detection}</Text>
+      )}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    flex: 1,
+    zIndex: 2,
+    backgroundColor: 'white',
+  },
+  camera:{
+    flex: 10,
+    width: '100%',
+    zIndex: 1,
+  },
+});
